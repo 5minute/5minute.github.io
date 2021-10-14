85a9dd252531fa0d5e95e841ed16b95d3fba9cdd
Sergey Trasko <sergey.trasko@gmail.com>
2021-10-14 17:46:55 +0300

    Cookie banner fixed

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,42 @@
-function showCookieBanner(){
-    let cookieBanner = document.getElementsByClassName("nk-cookie-banner")[0];
-    cookieBanner.style.display = "block";
-}
- 
-function hideCookieBanner(){
-    localStorage.setItem("web_dev_isCookieAccepted", "yes");
-    let cookieBanner = document.getElementsByClassName("nk-cookie-banner")[0];
-    cookieBanner.style.display = "none";
-}
-
-function initializeCookieBanner(){
-    let isCookieAccepted = localStorage.getItem("web_dev_isCookieAccepted");
-    if (isCookieAccepted === null) {
-        localStorage.clear();
-        localStorage.setItem("web_dev_isCookieAccepted", "no");
-        showCookieBanner();
+function initCookieBanner() {
+    function showCookieBanner(){
+        let cookieBanner = document.getElementsByClassName("nk-cookie-banner")[0];
+        cookieBanner.style.display = "block";
     }
-    if (isCookieAccepted === "no") {
-        showCookieBanner();
+     
+    function hideCookieBanner(){
+        localStorage.setItem("web_dev_isCookieAccepted", "yes");
+        let cookieBanner = document.getElementsByClassName("nk-cookie-banner")[0];
+        cookieBanner.style.display = "none";
     }
+    
+    function initializeCookieBanner(){
+        let isCookieAccepted = localStorage.getItem("web_dev_isCookieAccepted");
+        if (isCookieAccepted === null) {
+            localStorage.clear();
+            localStorage.setItem("web_dev_isCookieAccepted", "no");
+            showCookieBanner();
+        }
+        if (isCookieAccepted === "no") {
+            showCookieBanner();
+        }
+    }
+    
+    var interval = setInterval(function () {
+        if ($('#cookieBanner').length) {
+            clearInterval(interval);
+            initializeCookieBanner();
+        }
+    }, 100);
+    window.nk_hideCookieBanner = hideCookieBanner;    
 }
-
-window.onload = initializeCookieBanner();
-window.nk_hideCookieBanner = hideCookieBanner;
 
 $(function () {
     var includes = $('[data-include]')
     $.each(includes, function () {
       var file = $(this).data('include') + '.html'
       $(this).load(file);
-    })
+    });
+
+    initCookieBanner();
   });
